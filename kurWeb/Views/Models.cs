ae2224ff5481679bf8274f417a934785cf58c468
Yusuf Halil ÇATAL <y.halilc.37@gmail.com>
2024-07-04 11:38:45 +0300

    a

--- kurWeb/Views/Models.cs
+++ kurWeb/Views/Models.cs
@@ -3,7 +3,7 @@
     public class DataKur
     {
         public string Kodu { get; set; }
-        public string Asi { get; set; }
+        public string Adi { get; set; }
         public decimal AlisKuru { get; set; }
         public string SatisKuru { get; set; }
     }
